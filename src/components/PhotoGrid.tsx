512f40c2f729a7442e20f30ac7dcb78ade2a4a1d
aohus <dbtnghk528@gmail.com>
2025-12-04 22:11:22 +0900

    modify: use thumbnail in grid

--- src/components/PhotoGrid.tsx
+++ src/components/PhotoGrid.tsx
@@ -16,7 +16,7 @@ export function PhotoGrid({ photos }: PhotoGridProps) {
             {photos.map((photo) => (
               <div key={photo.id} className="aspect-square bg-gray-100 rounded-md overflow-hidden">
             <img
-              src={photo.url}
+              src={photo.thumbnail_path || photo.url}
               alt={photo.original_filename}
               className="w-full h-full object-cover transition-transform group-hover:scale-105"
               loading="lazy"
